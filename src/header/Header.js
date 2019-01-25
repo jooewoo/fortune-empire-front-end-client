bc30c1bf437debbc3d33b433b44425c4c66be9ff
jooewoo <jooewoo@gmail.com>
2019-01-25 01:45:55 -0500

    Adds the logo png file to the header folder

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from './fortune-empire.png'
 import './Header.scss'
 
 const authenticatedOptions = (
@@ -18,20 +18,16 @@ const unauthenticatedOptions = (
   </React.Fragment>
 )
 
-const alwaysOptions = (
-  <React.Fragment>
-    <Link to="/">Home</Link>
-  </React.Fragment>
-)
-
 const Header = ({ user }) => (
   <header className="main-header">
-    <img src="https://i.imgur.com/Q2iEfLb.png" height="90px" width="160px"/>
-    <h1>Fortune Empire</h1>
+    <h2>
+      <Link to="/">
+        <img src={logo}height="120px" width="390px"/>
+      </Link>
+    </h2>
     <nav>
       { user && <span>Welcome, {user.email}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
-      { alwaysOptions }
     </nav>
   </header>
 )
